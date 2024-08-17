1d837d26593f76c3fb9853d537c4df4fb5aa0983
JCGM <juliancamilo0210@gmail.com>
2024-08-17 15:43:21 -0500

    fix

--- src/main/java/io/riwi/weekly_workshop/WeeklyWorkshopApplication.java
+++ src/main/java/io/riwi/weekly_workshop/WeeklyWorkshopApplication.java
@@ -11,15 +11,4 @@ public class WeeklyWorkshopApplication {
 	}
 
 }
-package io.riwi.weekly_workshop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class WeeklyWorkshopApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(WeeklyWorkshopApplication.class, args);
-	}
-
-}
